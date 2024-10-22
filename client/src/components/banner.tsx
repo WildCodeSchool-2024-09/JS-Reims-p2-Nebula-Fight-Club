db0624869b59e9e033376db997a3789c543648a2
Elysande <sirinan@hotmail.fr>
2024-10-22 16:58:58 +0200

    made banner with title, updated css and main to test it

---
+++ client/src/components/banner.tsx
@@ -0,0 +1,11 @@
+function Backbanner() {
+  return (
+    <section className="topbar">
+      <header className="logo">
+        <img src="src/assets/images/006.png" alt="" />
+      </header>
+    </section>
+  );
+}
+
+export default Backbanner;
