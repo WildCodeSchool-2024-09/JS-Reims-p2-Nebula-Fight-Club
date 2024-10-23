bc8d482455af4468c608335541d4269fba3f2f27
salviamak <salvia.mak@gmail.com>
2024-10-23 17:06:55 +0200

    add footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import Body from "../src/components/Body";
 
 import "./App.css";
+import Footer from "./components/Footer.tsx";
 import Navbar from "./components/NavBar.tsx";
 import Backbanner from "./components/banner";
 
@@ -10,6 +11,7 @@ function App() {
       <Backbanner />
       <Navbar />
       <Body />
+      <Footer/>
     </>
   );
 }
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="item-footer">
+        <p>
+          Made by: Géraldine - Salvia - Thomas <br />
+          Wild Code School 2024
+        </p>
+      </div>
+      <div className="item-link">
+        <a href="géraldine-github.fr">Github Gégé</a>
+        <a href="salvia-github.fr">Github Salvia</a>
+        <a href="thomas-github.fr">Github Thomas</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
