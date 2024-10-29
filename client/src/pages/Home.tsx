dba9ebef13e86cfb5987b842dc5224bde8039e27
salviamak <salvia.mak@gmail.com>
2024-10-29 18:33:14 +0100

    update method getAllHeroes with variable env

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,10 +3,14 @@ import Card from "../components/Card";
 import FightButton from "../components/FightButton";
 import Intro from "../components/intro";
 import { getAllHeroes } from "../data/getAllHeroes";
+import useScreenSize from "../hooks/useScreenSize";
 import type { Heroe } from "../types/Hero";
 
 function Home() {
   const [heroes, setHeroes] = useState<Heroe[]>();
+  const { width } = useScreenSize();
+  const displayedCardsCount = width > 768 ? 8 : 6;
+
   useEffect(() => {
     const fetchHeroes = async () => {
       const data = await getAllHeroes();
@@ -20,13 +24,13 @@ function Home() {
     <div>
       <Intro />
       <div className="pokecards">
-        {heroes?.map((heroe) => {
+        {heroes?.slice(0, displayedCardsCount).map((heroe) => {
           return (
             <Card
               key={heroe.id}
               image={heroe?.image?.url}
               name={heroe?.name}
-              genre={heroe?.appearance.gender}
+              genre={heroe?.appearance?.gender}
             />
           );
         })}
