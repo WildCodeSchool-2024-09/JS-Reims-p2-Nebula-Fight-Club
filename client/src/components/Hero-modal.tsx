67094c76d9bc921283ea398a973fa2f8efc35adb
geraldine-l <geraldine.lonchay@gmail.com>
2024-10-30 17:08:24 +0100

    update css modale

--- client/src/components/Hero-modal.tsx
+++ client/src/components/Hero-modal.tsx
@@ -16,11 +16,31 @@ function HeroDetailsModal({
         onClick={(e) => e.stopPropagation()}
       >
         <button type="button" onClick={onClose} style={{ float: "right" }}>
-          Fermer
+          ❌
         </button>
+        <div className="modal-top-container">
+          <img className="modal-image" src={hero.image.url} alt={hero.name} />
+        </div>
+        <div className="Stat-hero">
+          <p>Vitesse = 20</p>
+          <p>Rapidité = 10</p>
+          <p>stat3 = 50</p>
+          <p>stat4 = 20</p>
+          <p>stat5 = 5</p>
+          <p>stat6 = 200</p>
+        </div>
         <h2>{hero.name}</h2>
-        <img src={hero.image.url} alt={hero.name} />
         <p>Comme l'a dit un grand philosophe... BASTOOOON !</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt sit
+          tempore, autem cumque similique possimus id itaque nulla minima
+          placeat?
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt sit
+          tempore, autem cumque similique possimus id itaque nulla minima
+          placeat?
+        </p>
       </div>
     </div>
   );
