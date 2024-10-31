a6c69425ae58a79cdcb48e54c669b424f1e20aae
geraldine-l <geraldine.lonchay@gmail.com>
2024-10-31 10:34:50 +0100

    fix erreur d'indentation

--- client/src/components/HeroModal.tsx
+++ client/src/components/HeroModal.tsx
@@ -20,28 +20,28 @@ function HeroDetailsModal({
         </button>
         <div className="modal-top-container">
           <img className="modal-image" src={hero.image.url} alt={hero.name} />
-        </div>
-        <div className="Stat-hero">
-          <p>Vitesse = 20</p>
-          <p>Rapidité = 10</p>
-          <p>stat3 = 50</p>
-          <p>stat4 = 20</p>
-          <p>stat5 = 5</p>
-          <p>stat6 = 200</p>
+          <div className="Stat-hero">
+            <p>Vitesse = 20</p>
+            <p>Rapidité = 10</p>
+            <p>stat3 = 50</p>
+            <p>stat4 = 20</p>
+            <p>stat5 = 5</p>
+            <p>stat6 = 200</p>
+          </div>
         </div>
         <div className="modal-bottom-container">
-        <h2>{hero.name}</h2>
-        <p>Comme l'a dit un grand philosophe... BASTOOOON !</p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt sit
-          tempore, autem cumque similique possimus id itaque nulla minima
-          placeat?
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt sit
-          tempore, autem cumque similique possimus id itaque nulla minima
-          placeat?
-        </p>
+          <h2>{hero.name}</h2>
+          <p>Comme l'a dit un grand philosophe... BASTOOOON !</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt
+            sit tempore, autem cumque similique possimus id itaque nulla minima
+            placeat?
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt
+            sit tempore, autem cumque similique possimus id itaque nulla minima
+            placeat?
+          </p>
         </div>
       </div>
     </div>
