647b1bcbe7ea7d4ccb2322892420f66c076c3537
Elysande <sirinan@hotmail.fr>
2024-11-07 16:57:50 +0100

    corrected a small error on import order

--- client/src/pages/SearchHero.tsx
+++ client/src/pages/SearchHero.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import Card from "../components/Card";
+import HeroDetailsModal from "../components/HeroModal";
 import { getHeroByName } from "../data/getHeroByName";
 import type { Heroe } from "../types/Hero";
+import "../components/Modal.css";
 
 function SearchHero() {
   const [name, setName] = useState("");
   const [heroes, setHeroes] = useState<Heroe[]>();
+  const [selectedHero, setSelectedHero] = useState<Heroe | null>(null);
+  const handleCardClick = (heroe: Heroe) => {
+    setSelectedHero(heroe);
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -22,14 +28,16 @@ function SearchHero() {
 
   return (
     <div className="search-bar">
-      <h3>Search</h3>
-      <input
-        type="text"
-        value={name}
-        onChange={handleChange}
-        placeholder="Recherche par nom"
-        className="input-search"
-      />
+      <div className="chocobon">
+        <h3>Search</h3>
+        <input
+          type="text"
+          value={name}
+          onChange={handleChange}
+          placeholder="Recherche par nom"
+          className="input-search"
+        />
+      </div>
       <div className="pokecards">
         {heroes && heroes.length >= 1 ? (
           heroes.map((heroe) => {
@@ -39,12 +47,18 @@ function SearchHero() {
                 image={heroe?.image?.url}
                 name={heroe?.name}
                 gender={heroe?.appearance.gender}
-                onClick={() => console.log("test")}
+                onClick={() => handleCardClick(heroe)}
               />
             );
           })
         ) : (
           <div style={{ color: "red" }}>Aucune donnée</div>
+        )}
+        {selectedHero && (
+          <HeroDetailsModal
+            hero={selectedHero}
+            onClose={() => setSelectedHero(null)}
+          />
         )}
       </div>
     </div>
