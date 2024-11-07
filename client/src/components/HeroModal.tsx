7ebb09e645e671b4a7bf004da19c6175e2f31f50
Elysande <sirinan@hotmail.fr>
2024-11-07 14:18:03 +0100

    updated css for modal card, media query done, need check up after next pull

--- client/src/components/HeroModal.tsx
+++ client/src/components/HeroModal.tsx
@@ -20,29 +20,36 @@ function HeroDetailsModal({
         onKeyDown={handleKeyDown}
         onClick={(e) => e.stopPropagation()}
       >
-        <button type="button" onClick={onClose} style={{ float: "right" }}>
+        <button
+          className="close-toto"
+          type="button"
+          onClick={onClose}
+          style={{ float: "right" }}
+        >
           ❌
         </button>
         <div className="modal-top">
           <img className="modal-image" src={hero.image.url} alt={hero.name} />
-          <h2>{hero.name}</h2>
+          <p>{hero.name}</p>
         </div>
         <section className="stats-container">
-          <ul className="offensive-stats">
-            <li>COMBAT : {hero.powerstats.combat}</li>
-            <li>STRENGTH : {hero.powerstats.strength}</li>
-            <li>POWER : {hero.powerstats.power}</li>
-          </ul>
-          <ul className="defensive-stats">
-            <li>SPEED : {hero.powerstats.speed}</li>
-            <li>INTELLIGENCE : {hero.powerstats.intelligence}</li>
-            <li>DURABILITY : {hero.powerstats.durability}</li>
-          </ul>
+          <div className="tartiflette">
+            <ul className="offensive-stats">
+              <li>COMBAT : {hero.powerstats.combat}</li>
+              <li>STRENGTH : {hero.powerstats.strength}</li>
+              <li>POWER : {hero.powerstats.power}</li>
+            </ul>
+            <ul className="defensive-stats">
+              <li>SPEED : {hero.powerstats.speed}</li>
+              <li>INTELLIGENCE : {hero.powerstats.intelligence}</li>
+              <li>DURABILITY : {hero.powerstats.durability}</li>
+            </ul>
+          </div>
           <div className="card-data">
             <p>Alignement : {hero.biography.alignment}</p>
             <p>{hero.biography.aliases}</p>
-            <h3>{hero.biography["first-appearance"]}</h3>
-            <h3>{hero.biography.publisher}</h3>
+            <p>{hero.biography["first-appearance"]}</p>
+            <p>{hero.biography.publisher}</p>
           </div>
         </section>
       </section>
