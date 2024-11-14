70c1e1b6fdf83fb2119f310bb060cf843a5e3ec4
Elysande <sirinan@hotmail.fr>
2024-11-14 17:33:25 +0100

    retake on the about page, made it works, still lots of placeholders

--- client/src/components/CardUs.tsx
+++ client/src/components/CardUs.tsx
@@ -1,28 +1,14 @@
 import "./About.css";
 
-interface CardProps {
+interface CardPropsUs {
   image: string;
   firstName: string;
   description: string;
 }
 
-function CardUs(props: CardProps) {
+function CardUs(props: CardPropsUs) {
   return (
     <section className="container">
-      <div className="body-card">
-        <img className="image-card" src={props.image} alt={props.firstName} />
-        <div className="about-us-container">
-          <p>{props.firstName}</p>
-          <p>{props.description}</p>
-        </div>
-      </div>
-      <div className="body-card">
-        <img className="image-card" src={props.image} alt={props.firstName} />
-        <div className="about-us-container">
-          <p>{props.firstName}</p>
-          <p>{props.description}</p>
-        </div>
-      </div>
       <div className="body-card">
         <img className="image-card" src={props.image} alt={props.firstName} />
         <div className="about-us-container">
--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,16 +1,112 @@
-import "../components/About.css";
-// import CardUs from "../components/CardUs";
+import { useEffect, useState } from "react";
+import CardUs from "../components/CardUs";
+import FightButton from "../components/FightButton";
+import { getOurHeroes } from "../data/getOurHeroes";
+import type { Heroe } from "../types/Hero";
 
 function About() {
-  return (
-    <div className="container-about">
-      <h1>Nous sommes la team GTS !</h1>
+  const [heroes, setHeroes] = useState<Heroe[]>();
+  useEffect(() => {
+    const fetchHeroes = async () => {
+      const data = await getOurHeroes();
+      setHeroes(data);
+    };
 
-      {/* <CardUs
-        image={props.image}
-        firstName={props.firstName}
-        description={props.description}
-      /> */}
+    fetchHeroes();
+  }, []);
+  return (
+    <div>
+      <section className="Greuh">
+        <h4> Gégé !</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet cumque
+          itaque inventore vero quas architecto est possimus nostrum hic,
+          temporibus iusto, ex, ipsam veniam! Repellendus, corrupti. Cupiditate
+          aliquam tenetur, deserunt cumque, non vero autem, necessitatibus
+          provident nihil vitae eaque magnam unde officiis. Corporis rem eum aut
+          iste accusantium ut harum.
+        </p>
+        <div className="pokecards">
+          <section className="ourpokecards">
+            <h3> Mes personnages préférés !</h3>
+            {heroes && heroes.length >= 2 && (
+              <>
+                <CardUs
+                  image={heroes[0].image?.url}
+                  firstName={heroes[0].name}
+                  description={"Tagazok"}
+                />
+                <CardUs
+                  image={heroes[1].image?.url}
+                  firstName={heroes[1].name}
+                  description={"Tagazok"}
+                />
+              </>
+            )}
+          </section>
+        </div>
+      </section>
+      <section className="Greuh">
+        <h4> Greuh !</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet cumque
+          itaque inventore vero quas architecto est possimus nostrum hic,
+          temporibus iusto, ex, ipsam veniam! Repellendus, corrupti. Cupiditate
+          aliquam tenetur, deserunt cumque, non vero autem, necessitatibus
+          provident nihil vitae eaque magnam unde officiis. Corporis rem eum aut
+          iste accusantium ut harum.
+        </p>
+        <div className="pokecards">
+          <section className="ourpokecards">
+            <h3> Mes personnages préférés !</h3>
+            {heroes && heroes.length >= 2 && (
+              <>
+                <CardUs
+                  image={heroes[2].image?.url}
+                  firstName={heroes[2].name}
+                  description={"Tagazok"}
+                />
+                <CardUs
+                  image={heroes[3].image?.url}
+                  firstName={heroes[3].name}
+                  description={"Tagazok"}
+                />
+              </>
+            )}
+          </section>
+        </div>
+      </section>
+      <section className="Greuh">
+        <h4> Salvia !</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet cumque
+          itaque inventore vero quas architecto est possimus nostrum hic,
+          temporibus iusto, ex, ipsam veniam! Repellendus, corrupti. Cupiditate
+          aliquam tenetur, deserunt cumque, non vero autem, necessitatibus
+          provident nihil vitae eaque magnam unde officiis. Corporis rem eum aut
+          iste accusantium ut harum.
+        </p>
+        <div className="pokecards">
+          <section className="ourpokecards">
+            <h3> Mes personnages préférés !</h3>
+            {heroes && heroes.length >= 2 && (
+              <>
+                <CardUs
+                  image={heroes[4].image?.url}
+                  firstName={heroes[4].name}
+                  description={"Tagazok"}
+                />
+                <CardUs
+                  image={heroes[5].image?.url}
+                  firstName={heroes[5].name}
+                  description={"Tagazok"}
+                />
+              </>
+            )}
+          </section>
+        </div>
+      </section>
+      <FightButton />
     </div>
   );
 }
